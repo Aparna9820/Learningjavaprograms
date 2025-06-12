6575e9d64e1252d56bbab3fcc3bfeee2cef474b1
Aparna9820 <aparnayedubati9820@gmail.com>
2025-06-12 11:02:57 +0530

    all programs

---
+++ src/May_10th_Class_Java_Basics/Lab_06.java
@@ -0,0 +1,7 @@
+package May_10th_Class_Java_Basics;
+
+public class Lab_06 {
+    public static void main(String[] args) {
+        System.out.println("Identifier with combination of letters & Numbers");
+    }
+}
---
+++ src/May_11th_Class_Variables_DataTypes/Lab_18_Byte_Interview.java
@@ -0,0 +1,5 @@
+package May_11th_Class_Variables_DataTypes;
+
+public class Lab_18_Byte_Interview {
+
+}
