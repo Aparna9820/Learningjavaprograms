11164eca28a8d43e548c3b013b16fb3ee6f7d5dd
Aparna9820 <aparnayedubati9820@gmail.com>
2025-06-21 12:19:15 +0530

    Q8

---
+++ src/Coding_test_Questions/Question8.java
@@ -0,0 +1,21 @@
+package Coding_test_Questions;
+//Write a program to check if a given year is a leap year.
+//
+//**Requirements:**
+//- A year is leap if it's divisible by 4
+//- Exception: if divisible by 100, it's not leap unless also divisible by 400
+//- Use logical operators in conditions
+public class Question8 {
+    public static void main(String[] args) {
+        int year=2024;
+        if ((year%4==0 && year%100!=100)|| (year%4==0)){
+            System.out.println(" Leap year ");
+        }
+        else
+            System.out.println("Not a leap year");
+    }
+
+}
+// Leap year
+//
+//Process finished with exit code 0
