9936c25e8f9c7d554c7638df631271a5f8f70086
Aparna9820 <aparnayedubati9820@gmail.com>
2025-06-21 11:25:06 +0530

    Q1

---
+++ src/Coding_test_Questions/Question1.java
@@ -0,0 +1,30 @@
+package Coding_test_Questions;
+
+public class Question1 {
+        public static void main(String[] args) {
+            // Integer variable
+            int age = 25;
+
+            // Double variable
+            double salary = 55000.75;
+
+            // Boolean variable
+            boolean isEmployed = true;
+
+            // Character variable
+            char grade = 'A';
+
+            // Printing the values
+            System.out.println("Age: " + age);
+            System.out.println("Salary: " + salary);
+            System.out.println("Is Employed: " + isEmployed);
+            System.out.println("Grade: " + grade);
+        }
+    }
+
+//Age: 25
+//Salary: 55000.75
+//Is Employed: true
+//Grade: A
+
+//Process finished with exit code 0
