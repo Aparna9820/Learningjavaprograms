1adeb4fa1e8c98fe6905a7377d980418c8e2a6dd
Aparna9820 <aparnayedubati9820@gmail.com>
2025-06-21 12:10:00 +0530

    Q6

---
+++ src/Coding_test_Questions/Question6.java
@@ -0,0 +1,29 @@
+package Coding_test_Questions;
+
+import java.util.Scanner;
+
+//Write a program to check if a number is positive, negative, or zero.
+//
+//**Requirements:**
+//- Read an integer from user input
+//- Use if-else statements to classify the number
+//- Print appropriate message for each case
+public class Question6 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter Any Number: ");
+        int num=scanner.nextInt();
+        if (num>0) {
+            System.out.println("Given Number is Positive Number");
+        }else if(num<0) {
+            System.out.println("Given Number is Negetive Number");
+        }else
+            System.out.println("Given number is Zero-->0");
+
+    }
+}
+//Enter Any Number:
+//-90
+//Given Number is Negetive Number
+//
+//Process finished with exit code 0
